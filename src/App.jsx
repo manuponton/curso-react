25c67570fc260c888e991944cb011cfcdee5cf99
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-06-30 15:09:53 -0500

    New component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola, soy el un nuevo componente</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hola.jsx
@@ -0,0 +1,7 @@
+import React, {Fragment} from "react";
+function Hola() {
+    <Fragment>
+        <h1>Hola, soy un nuevo componente</h1>
+    </Fragment>
+}
+export default  Hola
